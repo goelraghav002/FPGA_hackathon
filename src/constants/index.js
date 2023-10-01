5144704f465ec84fc1b309365d4a92e802e209e5
Raghav Goel <goelraghav002@gmail.com>
2023-10-01 16:22:07 +0530

    timeline update

--- src/constants/index.js
+++ src/constants/index.js
@@ -20,3 +20,26 @@ export const organizers = [
     img: aujus,
   },
 ];
+
+export const timeline = [
+    {
+        date: "3rd October 2023",
+        event: "Orientiation",
+    },
+    {
+        date: "10th October 2023",
+        event: "Team Registration"
+    },
+    {
+        date: "12th October 2023",
+        event: "Idea Shortlisting",
+    },
+    {
+        date: "26th October 2023",
+        event: "12 Hours Offline Hackathon"
+    },
+    {
+        date: "26th October 2023",
+        event: "Idea Presentation"
+    }
+]
