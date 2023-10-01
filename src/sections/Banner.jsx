e40f5c216e8e30cd58f25074c05eced346b03e22
kartik <kartikaggarwal2004@gmail.com>
2023-10-01 16:24:42 +0530

    made the banner

--- src/sections/Banner.jsx
+++ src/sections/Banner.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import './Banner.scss'
+import Typewriter from 'typewriter-effect';
+import {BsDiscord} from 'react-icons/bs'
+import {AiOutlineInstagram, AiOutlineTwitter} from 'react-icons/ai'
+import {BiLogoLinkedin} from 'react-icons/bi'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+  
+    <div className="landing">      
+        <div className='content'>
+          <h1>Hack Street</h1>
+          <span className='typewriter'>
+            <Typewriter
+              options={{
+                strings: ['48 hours of creativity', 'Code In the Abyssal'],
+                autoStart: true,
+                loop: true,
+              }}
+              
+            />
+          </span>
+
+          <div className="banner-link">
+            <p>on 2nd and 3rd october</p>
+            <a href="" className='join_disco'><BsDiscord /> &nbsp; &nbsp; Join our discord</a>
+
+            <a href="" className='devfolio'>Go to Projects</a>
+ 
+            
+          </div>
+
+          <div className="bottom">
+            <p>Follow us on social media for updates</p>
+            <ul>
+              <li><a href=""><AiOutlineInstagram /></a></li>
+              <li><a href=""><BsDiscord /></a></li>
+              <li><a href=""><BiLogoLinkedin /></a></li>
+              <li><a href=""><AiOutlineTwitter /></a></li>
+            </ul>
+          </div>
+
+        </div>
+
+
+      </div>          
+    
   )
 }
 
