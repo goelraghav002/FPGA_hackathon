872390ebf6284795cd081d0bf00c1b6a122df5ba
kartik <kartikaggarwal2004@gmail.com>
2023-10-01 22:14:35 +0530

    made the timeline section and banner is now responsive

--- src/sections/Timeline.jsx
+++ src/sections/Timeline.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import './Timeline.scss'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import {timeline} from "../constants/index"
+import {AiFillCalendar} from "react-icons/ai"
 
 const Timeline = () => {
+
+
   return (
-    <div>Timeline</div>
-  )
+    <div className="timeline-section">
+      <VerticalTimeline lineColor='#3498db'>
+
+        {timeline.map((item, index) => {
+          return (
+            <VerticalTimelineElement
+              key={index}
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: '#f5f5f5', color: '#333333' }}
+              contentArrowStyle={{ borderRight: '7px solid  #f5f5f5 ' }}
+              iconStyle={{ background: '#3498db', color: '#fff' }}        
+              icon={<AiFillCalendar />}
+            >
+              <h3 className="vertical-timeline-element-title">{item.date}</h3>              
+              <p style={{color:" #777777"}}>
+                {item.event}
+              </p>
+            </VerticalTimelineElement>
+          )
+        })
+      } 
+      </VerticalTimeline>
+    </div>
+  );
 }
+
+
+
 
 export default Timeline
