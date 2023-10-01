b989bfcbc4084bb53ed11562a0a1ea9100b3dbdc
Raghav Goel <goelraghav002@gmail.com>
2023-10-01 14:52:00 +0530

    added organizers details

---
+++ src/assets/organizers/index.js
@@ -0,0 +1,6 @@
+import amd from "./amd.png";
+import jiit from "./jiit.png";
+import aujus from "./aujus.png";
+import ieee from "./ieee.png";
+
+export { amd, jiit, aujus, ieee };
---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+import { ieee, jiit, amd, aujus} from "../assets/organizers"; 
+
+export const prizes = [{}, {}, {}, {}, {}];
+
+export const organizers = [
+  {
+    name: "ECE Department JIIT",
+    img: jiit,
+  },
+  {
+    name: "IEEE SB JIIT",
+    img: ieee,
+  },
+  {
+    name: "AMD",
+    img: amd,
+  },
+  {
+    name: "Aujus Technology",
+    img: aujus,
+  },
+];
